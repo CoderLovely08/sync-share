3680406149c1e411520e20befecad6092c7db395
Lovely Sharma <lovelysingeras297@gmail.com>
2024-02-21 02:12:39 +0530

    Base landing page compeletd

---
+++ client/src/pages/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+      <div className='w-full container'>
+          Landing page
+    </div>
+  )
+}
+
+export default LandingPage
